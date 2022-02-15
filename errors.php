d42e31a37236273ae27887a9da6e09cb4ffd53f5
Symwe <98308521+Symwe@users.noreply.github.com>
2022-02-15 13:07:00 +0300

    Added CSS and php files

---
+++ errors.php
@@ -0,0 +1,9 @@
+<?php include_once "register.php";?>
+
+<?php  if (count($errors) > 0) : ?>
+  <div class="error">
+  	<?php foreach ($errors as $error) : ?>
+  	  <p><?php echo $error ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
